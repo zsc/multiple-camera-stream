31ccae45ce06e105bfe05888aecc48664e330a49
Shuchang Zhou <zsc@haidian>
2020-02-02 01:19:39 +0800

    Delay RGB 2s to match in time.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, Response
 import cv2
+import numpy as np
 
 app = Flask(__name__)
 
@@ -11,29 +12,39 @@ def find_camera(id):
 #  for webcam use zero(0)
  
 
-def gen_frames(camera_id):
+def gen_frames():
      
-    cam = find_camera(camera_id)
-    cap=  cv2.VideoCapture(cam)
+    cam = find_camera(0)
+    cap = cv2.VideoCapture(cam)
+    cam2 = find_camera(1)
+    cap2 = cv2.VideoCapture(cam2)
+
+    num_frame = 50
+    buf = []
     
     while True:
         # for cap in caps:
         # # Capture frame-by-frame
         success, frame = cap.read()  # read the camera frame
-        if not success:
+        success2, frame2 = cap2.read()  # read the camera frame
+        if not success or not success2:
             break
         else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
+            buf.append(frame2)
+            if len(buf) > num_frame:
+                buf = buf[-num_frame:]
+                frame2 = buf[0]
+                img = np.concatenate([cv2.resize(frame, (int(frame2.shape[0] / frame.shape[0] * frame.shape[1]), frame2.shape[0])), frame2], axis=1)
+                ret, buffer = cv2.imencode('.jpg', img)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 @app.route('/video_feed/<string:id>/', methods=["GET"])
 def video_feed(id):
    
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen_frames(id),
+    return Response(gen_frames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
